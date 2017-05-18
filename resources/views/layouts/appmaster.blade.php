4a0d21a2c9c0b332a39a66f44a447f611aae0a52
Mark Reha <mark@Marks-MacBook-Pro.local>
2017-05-18 11:08:20 -0700

    Added datetime picker

--- resources/views/layouts/appmaster.blade.php
+++ resources/views/layouts/appmaster.blade.php
@@ -2,12 +2,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link href="resources/assets/css/weather.css" rel="stylesheet" type="text/css" >
+
+    <script type="text/javascript" src="http://code.jquery.com/ui/1.11.0/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="resources/assets/js/jquery-ui-timepicker-addon.js"></script>
+    <script type="text/javascript" src="resources/assets/js/jquery-ui-timepicker-addon-i18n.min.js"></script>
+    <script type="text/javascript" src="resources/assets/js/jquery-ui-sliderAccess.js"></script>
+    <link rel="stylesheet" type="text/css" href="resources/assets/css/jquery-ui-timepicker-addon.css">
+	<link rel="stylesheet" media="all" type="text/css" href="http://code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css" />
+    
     <title>@yield('title')</title>
 </head>
 
